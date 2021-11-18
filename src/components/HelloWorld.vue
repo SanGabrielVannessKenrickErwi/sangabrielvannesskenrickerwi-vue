7b811f910b45b5a64e386a24ca48f5330ff781b7
SanGabrielVannessKenrickErwi <gabrielsan147@mhs.mdp.ac.id>
2021-11-18 18:41:06 +0700

    belajar router view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello">
-  </div>
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
@@ -56,8 +55,7 @@
         <footer class="py-5 bg-dark">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
         </footer>
-        
-  
+    </div>
 </template>
 
 <script>
