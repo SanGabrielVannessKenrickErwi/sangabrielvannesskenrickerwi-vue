9301fff195eceb4d6ccfe18646bba7e93ead7209
SanGabrielVannessKenrickErwi <gabrielsan147@mhs.mdp.ac.id>
2021-12-04 18:17:14 +0700

    LayoutMain

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,55 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
+
 const routes = [
     {
         path: "/",
         name: "home",
-        component: () => import('@/components/HelloWorld.vue')
+        component: () => import('@/components/HelloWorld')
     },
     {
         path: "/login",
         name: "login",
-        component: () => import('@/views/login/Main.vue')
+        component: () => import('@/views/login/Main')
     },
     {
         path: '/register',
         name: 'register',
-        component: () => import('@/views/register/Main.vue')
-    }
+        component: () => import ('@/views/register/Main')
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard/Main')
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/profile/Main')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('@/views/about/Main')
+    },
+    {
+        path: '/users',
+        name: 'users',
+        component: () => import('@/views/users/Main')
+    },
+    {
+        path: '/users/:id/:username?',
+        name: 'detailuser',
+        component: () => import('@/views/users/DetailUser')
+    },
+    {
+        path: '/create-user',
+        name: 'createuser',
+        component: () => import('@/views/users/FormCreate')
+    },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 });
 export default router;
